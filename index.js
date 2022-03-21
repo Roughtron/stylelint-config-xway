9c155bf35c64d2bd59501f7a7ed9a56a88e220f0
Roughtron <roughtron@yandex.ru>
2022-03-21 09:20:17 +0300

    add packages

--- index.js
+++ index.js
@@ -2,5 +2,11 @@ module.exports = {
   extends: [
     "stylelint-config-standard",
     "stylelint-plugin-stylus/standard",
-  ]
+    "stylelint-config-rational-order"
+  ],
+  rules: {
+    'no-descending-specificity': null,
+    'selector-class-pattern': null,
+    'stylus/declaration-colon': null
+  }
 };
